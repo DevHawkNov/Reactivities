fc69dc6d6b883d67d1fccae1e56e5975e0e45dac
EK <eaglecrest.mark@gmail.com>
2025-09-15 13:10:46 -0400

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -11,20 +11,17 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 // builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.MapOpenApi();
-// }
-
-// app.UseHttpsRedirection();
-
-// app.UseAuthorization();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().
+    WithOrigins("http://localhost:5173", "https://localhost:5173"));
 
 app.MapControllers();
 
